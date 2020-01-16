a839bfd66ec8d63c02f2c0e2fc371c60fc81a48b
kpatil <kpatil@salesforce.com>
2020-01-16 13:41:18 -0800

    interfaces in go

---
+++ examples/interfaces.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+
+// ----------- rectangle object
+type rect struct {
+	width, height float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+
+func (r rect) perim() float64 {
+	return 2*r.width + 2*r.height
+}
+
+/// circle
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+// not method
+func measure(g geometry) {
+	fmt.Println(g, g.area(), g.perim())
+}
+
+func main() {
+	r := rect{width: 3, height: 4}
+	c := circle{radius: 5}
+	// The circle and rect struct types both implement
+	// the geometry interface so we can use instances of
+	// of these structs as arguments to measure()
+	measure(r)
+	measure(c)
+}
